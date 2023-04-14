54253cf73ae4c46a7eb58fd8628dc8c877a31f53
chetan <chetansalunke019317@gmail.com>
2023-04-14 19:45:17 +0530

    first commit

---
+++ src/components/newarrivals/Ndata.js
@@ -0,0 +1,34 @@
+const Ndata = [
+  {
+    cover: "./AutoGeek-Parts/images/arrivals/arrivals1.png",
+    name: "Sunglass",
+    price: "150",
+  },
+  {
+    cover: "./AutoGeek-Parts/images/arrivals/arrivals2.png",
+    name: "Makeup",
+    price: "250",
+  },
+  {
+    cover: "./AutoGeek-Parts/images/arrivals/arrivals3.png",
+    name: "Smart Watch",
+    price: "50",
+  },
+  {
+    cover: "./AutoGeek-Parts/images/arrivals/arrivals4.png",
+    name: "Lipstick",
+    price: "15",
+  },
+  {
+    cover: "./AutoGeek-Parts/images/arrivals/arrivals5.png",
+    name: "Green Plant",
+    price: "10",
+  },
+  {
+    cover: "./AutoGeek-Parts/images/arrivals/arrivals6.png",
+    name: "Bonsai tree",
+    price: "400",
+  },
+]
+
+export default Ndata
